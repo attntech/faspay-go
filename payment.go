30b36927886bc1158a2476c48a9ab49a74cfb7eb
Rizky isnindaputra <rizkyisninda@gmail.com>
2020-01-14 16:03:51 +0700

    fix: update payment notification response

--- payment.go
+++ payment.go
@@ -18,15 +18,14 @@ type PaymentNotification struct {
 }
 
 type PaymentNotificationResponse struct {
-	Response     string        `json:"response"`
-	TrxID        string        `json:"trx_id"`
-	MerchantID   string        `json:"merchant_id"`
-	Merchant     string        `json:"merchant"`
-	BillNo       string        `json:"bill_no"`
-	BillItems    []interface{} `json:"bill_items"`
-	ResponseCode string        `json:"response_code"`
-	ResponseDesc string        `json:"response_desc"`
-	RedirectURL  string        `json:"redirect_url"`
+	Response     string `json:"response"`
+	TrxID        string `json:"trx_id"`
+	MerchantID   string `json:"merchant_id"`
+	Merchant     string `json:"merchant"`
+	BillNo       string `json:"bill_no"`
+	ResponseCode string `json:"response_code"`
+	ResponseDesc string `json:"response_desc"`
+	ResponseDate string `json:"response_date"`
 }
 
 type Item struct {
